cd75cae4d4927dc28870566286d87df5faf64d04
Md Mamun <juborajvai22@gmail.com>
2022-05-23 20:02:15 +0600

    update

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 
 // middale ware 
 app.use(cors())
@@ -17,20 +19,42 @@ const uri = `mongodb+srv://${process.env.NAME}:${process.env.PASSWORD}@admin.tk0
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+function verifayJwt(req , res , next){
+      const authHeader = req.headers.authorization
+
+      if(!authHeader){
+            return res.status(401).send({ massage: 'Unauthorization Access' })
+      }
+
+      const token = authHeader.split(' ')[1]
+      jwt.verify(token, process.env.ACCESS_TOKEEN, function(err, decoded) {
+            if(err){
+                  return res.status(403).send({ massage: 'Forbidden Access' })
+            }
+            req.decoded = decoded
+            next()
+          });
+
+} 
+
+
 async function run(){
       try{
             await client.connect()
             const productCollection = client.db("manufacture").collection("product");
             const bookingCollection = client.db("manufacture").collection("bookings");
+            const userCollection = client.db("manufacture").collection("users");
 
-            // read data 
-            app.get('/product' , async (req , res)=>{
+
+
+            // read data -------
+            app.get('/product'  , async (req , res)=>{
                   const query = {}
                   const product = await productCollection.find(query).toArray()
                   res.send(product)     
             })
 
-            // read data specific 
+            // read data specific ----------
             app.get('/product/:id' , async (req , res) =>{
                   const id = req.params.id
                   const query = {_id: ObjectId(id)}
@@ -38,7 +62,7 @@ async function run(){
                   res.send(result)
             })
 
-            // updateQunetity 
+            // updateQunetity -----------
             app.put('/products/:id' , async (req , res)=>{
                   const id = req.params.id
                   const quentity = req.body
@@ -55,15 +79,38 @@ async function run(){
 
             })
 
-            // post oder 
-            app.post('/booking' , async (req , res) =>{
+            // post oder ------------
+            app.post('/booking' ,verifayJwt , async (req , res) =>{
                   const booking = req.body
                   const result = await bookingCollection.insertOne(booking)
                   res.send({message: "Your Order SuccessFull"})
             })
+
+            // create user ................
+            app.put('/user/:email' , async (req , res) =>{
+                  const email = req.params.email
+                  const user = req.body
+                  const filter = { email : email}
+                  const options = { upsert: true };
+                  const updateDoc = {
+                        $set: user
+                  }
+                  // create jwt token -----------
+                  const token = jwt.sign({email: email}, process.env.ACCESS_TOKEEN  , { expiresIn: '1h' });
+
+                  const result = await userCollection.updateOne(filter, updateDoc, options);
+
+                  res.send({result , token})
+                  
+
+
+            })
+
             
 
       }
+
+
       
       finally{
 
